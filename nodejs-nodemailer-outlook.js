fabed3f19d092ee09a321745132da194b00b5b12
youans <youans.ezzat@gmail.com>
2021-05-18 18:44:10 +0200

    Revert back some changes and upgrade nodemailer version

--- nodejs-nodemailer-outlook.js
+++ nodejs-nodemailer-outlook.js
@@ -1,38 +1,39 @@
-let nodemailer = require("nodemailer");
+var nodemailer = require('nodemailer');
 
 module.exports.sendEmail = function (options) {
-  const invok = function(f,x) {
-    f(x);
-  };
-  return new Promise((resolve, reject) => {
-    if (!options) {
-      reject("Options can not be null");
-    } else if (!options.auth) {
-      reject("Options.auth{user,pass} can not be null");
-    } else if (!options.auth.user || !options.auth.pass) {
-      reject("Options.auth.user or Options.auth.password can not be null");
+    if(!options){
+        throw new Error("Options can not be null");
+    } else if(!options.auth) {
+        throw new Error("Options.auth{user,pass} can not be null");
+    } else if(!options.auth.user || !options.auth.pass) {
+        throw new Error("Options.auth.user or Options.auth.password can not be null");
+
     }
     var transporter = nodemailer.createTransport({
-      host: options.host || "smtp.office365.com", // Office 365 server
-      port: options.port || 587, // secure SMTP
-      secure: options.secure || false, // false for TLS - as a boolean not string - but the default is false so just remove this completely
-      auth: options.auth,
-      tls: options.tls || { ciphers: "SSLv3" },
+        host: options.host || 'smtp.office365.com', // Office 365 server
+        port: options.port || 587,     // secure SMTP
+        secure:options.secure || false, // false for TLS - as a boolean not string - but the default is false so just remove this completely
+        auth: options.auth,
+        tls: options.tls || {ciphers: 'SSLv3'}
     });
-    transporter.sendMail(
-      {
+    transporter.sendMail({
         from: options.from,
+        sender: options.sender,
         replyTo: options.replyTo,
         to: options.to,
         subject: options.subject,
-        cc: options.cc,
-        bcc: options.bcc,
-        text: options.text,
-        html: options.html,
-        attachments: options.attachments,
-      },
-      // error, info callback
-      (e, i) => { invok(e ? reject : resolve, e ? e : i) }
-    );
-  });
-};
+        cc:options.cc,
+        bcc:options.bcc,
+        text:options.text,
+        html:options.html,
+        attachments:options.attachments,
+    }, function (err, info) {
+        if (err && options.onError) {
+            options.onError(err);
+        }
+        else if(options.onSuccess) {
+            options.onSuccess(info);
+        }
+    });
+}
+
